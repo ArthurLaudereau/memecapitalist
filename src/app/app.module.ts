6d0a567600fbd86c1aa18c7082e924c084319aa2
Arthur <arthur.laudereau@gmail.com>
2018-03-19 14:46:29 +0100

    Revert "progressbar début"
    This reverts commit f66283a66bf66a086c5ee485405a673710d9ef13.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { RestserviceService } from './restservice.service';
-import { BigvaluePipe } from './bigvalue.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent,
-    BigvaluePipe
+    ProductComponent
   ],
   imports: [
     BrowserModule, HttpModule
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../world';
 
 @Component({
@@ -8,10 +8,6 @@ import { Product } from '../world';
 })
 export class ProductComponent implements OnInit {
   product: Product;
-  progressbar: any;
-  @ViewChild('bar') progressBarItem;
-  
-  
   @Input()
   set prod(value: Product) {
     this.product = value;
@@ -19,10 +15,6 @@ export class ProductComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.progressbar = new
-      this.progressbar.Line(this.progressBarItem.nativeElement, { strokeWidth: 50, color: '#00ff00' });
-      this.progressbar.animate(1, { duration: this.product.vitesse });
-      //this.progressbar.set(progress); pour repartir avec le timeleft
-    }
+  }
 
 }
