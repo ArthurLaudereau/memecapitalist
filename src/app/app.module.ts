f66283a66bf66a086c5ee485405a673710d9ef13
Arthur <arthur.laudereau@gmail.com>
2018-03-19 12:30:47 +0100

    progressbar début
    cannot load list of products ??

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { RestserviceService } from './restservice.service';
+import { BigvaluePipe } from './bigvalue.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    BigvaluePipe
   ],
   imports: [
     BrowserModule, HttpModule
